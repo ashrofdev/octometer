76c3dd97ae9593702142292d2287d196f3fe9578
First Last <salmanashraf853@gmail.com>
2020-08-19 09:25:32 +0100

    setting up dev env

---
+++ src/Dashboards/Stakeholders.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Stakeholders extends Component {
+    render() {
+        return (
+            <div>
+                stakes
+            </div>
+        );
+    }
+}
+
+export default Stakeholders;
---
+++ src/Dashboards/Subscribers.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Subscribers extends Component {
+    render() {
+        return (
+            <div>
+                subsss
+            </div>
+        );
+    }
+}
+
+export default Subscribers;
